2838c07638f74a34374215f9acd886d178d4ceeb
julia <julia.anaya@gmail.com>
2012-05-19 20:08:58 +0200

    Etherpad API key now read from another file

--- config/initializers/etherpad-lite.rb
+++ config/initializers/etherpad-lite.rb
@@ -6,5 +6,6 @@
 require 'etherpad-lite'
 
 # This matches the API key defined in etherpad-lite/APIKEY.txt
+# Copy the key to config/etherpad-api-key.txt
 # Note: you could as well read that file if your rails user can do it.
-ETHERPAD_API_KEY="CHANGE_ME_TO_MATCH_ETHERPAD_API_KEY"
+ETHERPAD_API_KEY = IO.read(File.expand_path('../../etherpad-api-key.txt', __FILE__))
