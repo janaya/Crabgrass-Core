ab4f543b7331624bd63a1336d192fdc984d2ad59
julia <julia.anaya@gmail.com>
2012-05-19 21:48:38 +0200

    Create the actual pad after creating a PadPage

--- app/models/observers/page_observer.rb
+++ app/models/observers/page_observer.rb
@@ -1,5 +1,9 @@
 class PageObserver < ActiveRecord::Observer
 
+  def after_create(page)
+    EPL.sync!(page) if 'PadPage' == page.type # save pad to Etherpad-Lite
+  end
+
   def after_destroy(page)
     PageNotice.destroy_all_by_page(page)
   end
