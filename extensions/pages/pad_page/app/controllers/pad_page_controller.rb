1b53ab93085e78492a9f46c302a249f4a875a6d1
julia <julia.anaya@gmail.com>
2012-06-22 00:28:17 +0900

    added tests for PagPageController and a few minor fixes

--- extensions/pages/pad_page/app/controllers/pad_page_controller.rb
+++ extensions/pages/pad_page/app/controllers/pad_page_controller.rb
@@ -1,9 +1,7 @@
 class PadPageController < Pages::BaseController
 
   permissions :pages
-  guard_like 'page'
 
-  before_filter :login_required
   before_filter :refresh_epl_session, :only => [:show, :print]
   
   ##
@@ -18,17 +16,18 @@ class PadPageController < Pages::BaseController
   rescue Errno::ECONNREFUSED
     error("Connection to Etherpad-Lite failed: service unavailable.", :now)
   rescue Exception => e
-    info "NO ETHERPAD SESSION! #{e.class}: #{e.message}" # should deny permission?
+    error "NO ETHERPAD SESSION! #{e.class}: #{e.message}" # should deny permission?
   end
 
   # Set the EtherpadLite session cookie.
   # This will automatically be picked up by the jQuery plugin's iframe.
   def save_ep_session_cookie(sess_id)
-    cookies[:sessionID] = {
+    cookies['sessionID'] = {
       :value    => sess_id,
       :domain   => request.host,
       :path     => '/',
-      :expires  => Time.now + 60 * EPL::ETHERPAD_SESSION_DURATION
+      :expires  => Time.now + 60 * EPL::ETHERPAD_SESSION_DURATION,
+      :secure   => request.ssl?
     }
   end
 
--- extensions/pages/pad_page/test/functional/pad_page_controller_test.rb
+++ extensions/pages/pad_page/test/functional/pad_page_controller_test.rb
@@ -1,14 +1,59 @@
 require File.dirname(__FILE__) + '/../../../../../test/test_helper'
 
 class PadPageControllerTest < ActionController::TestCase
-  fixtures :pages, :users, :user_participations, :pads
 
   def setup
+    @user = User.make
+    @page = PadPage.create! :title => 'testing pad', :user => @user
   end
 
-  def test_flunk
-    # FIXME: missing tests!
-    flunk("PadPageController wants tests...")
+  def test_permission_denied_if_not_logged_in
+    get :show, :page_id => @page.id
+    assert_response :redirect
+    assert_redirected_to login_url
+    assert_nil assigns[:page]
+    assert_nil cookies['sessionID']
+  end
+
+  def test_permission_denied_without_access_to_page
+    login_as @user
+    @user.expects(:may?).with(:view, @page).returns(false)
+    assert_permission_denied do
+      assert_permission(:may_show_page?, false) do 
+        get :show, :page_id => @page.id
+        assert_response :success
+        assert_nil session[:ep_sessions]
+        assert_nil cookies['sessionID']
+      end
+    end
+  end
+
+  def test_show_sets_sessionID_for_etherpad
+    login_as @user
+    EPL.expects(:update_session!).returns(stub(:id => 'session id from etherpad'))
+    assert_permission(:may_show_page?)  do 
+      get :show, :page_id => @page.id
+      assert_response :success
+      assert_not_nil session[:ep_sessions]
+      assert_not_nil cookies['sessionID']
+      assert_equal 'session id from etherpad', cookies['sessionID']
+    end
+  end 
+
+  def test_connection_refused_shows_error_message
+    login_as @user
+    EPL.expects(:update_session!).raises(Errno::ECONNREFUSED)
+    get :show, :page_id => @page.id
+    assert_response :success
+    assert_error_message # equal 'test', flash
+  end
+
+  def test_etherpad_error_shows_error_message
+    login_as @user
+    EPL.expects(:update_session!).raises(Exception)
+    get :show, :page_id => @page.id
+    assert_response :success
+    assert_error_message # equal 'test', flash
   end
 
 end
